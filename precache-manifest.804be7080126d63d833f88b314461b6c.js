48efaee57adbe90d9db5516400d2d57f5bd234e1
isauadh <saugat.adhikari@ymail.com>
2019-08-11 00:44:24 -0500

    First commit

---
+++ precache-manifest.804be7080126d63d833f88b314461b6c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48e8b3b785d3f622c823d6a195963749",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dad5a342fbbbccefd9c8",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "9acd25bd8595d44540b6",
+    "url": "/static/js/2.09ba0d9e.chunk.js"
+  },
+  {
+    "revision": "dad5a342fbbbccefd9c8",
+    "url": "/static/js/main.ed3c19fc.chunk.js"
+  },
+  {
+    "revision": "c0a09d4cf98b4c1e824c",
+    "url": "/static/js/runtime~main.ff16ad5b.js"
+  }
+]);
