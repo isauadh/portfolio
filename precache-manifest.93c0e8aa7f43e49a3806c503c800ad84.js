a09f7f4255665fa680a2cf3c69d8beab2bd0fb5f
isauadh <saugat.adhikri@ttu.edu>
2019-08-21 13:21:20 -0400

    added build

---
+++ precache-manifest.93c0e8aa7f43e49a3806c503c800ad84.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd6688b0383eb0cd228a1912194c201b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb53da56568ef4d9437b",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "9acd25bd8595d44540b6",
+    "url": "/static/js/2.09ba0d9e.chunk.js"
+  },
+  {
+    "revision": "cb53da56568ef4d9437b",
+    "url": "/static/js/main.b0dd4295.chunk.js"
+  },
+  {
+    "revision": "c0a09d4cf98b4c1e824c",
+    "url": "/static/js/runtime~main.ff16ad5b.js"
+  }
+]);
